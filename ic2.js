d89e68ecd841454be5cf034448d74fbaece289c4
Danny Bai <baidanny091@gmail.com>
2021-03-02 00:19:01 -0500

    started ic2

---
+++ ic2.js
@@ -0,0 +1,5 @@
+// Print Low, Return High - Create a function that takes in an array of numbers.  The function should print the lowest value in the array, and return the highest value in the array.
+
+function printLowReturnHi(){
+    
+}
